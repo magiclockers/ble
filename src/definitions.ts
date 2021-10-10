6c7cf5cb0ac6f3f2953af6f9b5cb73571bfc0936
izumi <kinizumi@gmail.com>
2021-10-10 01:47:04 -0400

    update register entry

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,4 +1,4 @@
-export interface mixdemoPlugin {
+export interface MixdemoPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
   unlock(options: any):Promise<any>;
 }
--- src/web.ts
+++ src/web.ts
@@ -1,8 +1,30 @@
-import {WebPlugin} from '@capacitor/core';
+// import {WebPlugin} from '@capacitor/core';
+//
+// import type {mixdemoPlugin} from './definitions';
+//
+// export class mixdemoWeb extends WebPlugin implements mixdemoPlugin {
+//   async echo(options: { value: string }): Promise<{ value: string }> {
+//     console.log('ECHO', options);
+//     return options;
+//   }
+//
+//   async unlock(options: { snInfo: string, sdkKey: string }): Promise<any> {
+//     console.log(options);
+//   }
+// }
 
-import type {mixdemoPlugin} from './definitions';
 
-export class mixdemoWeb extends WebPlugin implements mixdemoPlugin {
+import {registerPlugin, WebPlugin} from '@capacitor/core';
+
+import type {MixdemoPlugin} from './definitions';
+
+export class MixdemoWeb extends WebPlugin implements MixdemoPlugin {
+  constructor() {
+    super({
+      name: 'MixdemoPlugin',
+      platforms: ['web']
+    });
+  }
   async echo(options: { value: string }): Promise<{ value: string }> {
     console.log('ECHO', options);
     return options;
@@ -12,3 +34,10 @@ export class mixdemoWeb extends WebPlugin implements mixdemoPlugin {
     console.log(options);
   }
 }
+
+const MixPlugin = registerPlugin<MixdemoPlugin>('MixdemoPlugin', {
+  web: () => import('./web').then(m => new m.MixdemoWeb()),
+});
+
+export * from './definitions';
+export { MixPlugin };
